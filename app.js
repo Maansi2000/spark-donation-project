bcce5b7bc8bcd04e7fa35c6c6ede579bb0c98ac8
maansi <maansijha.2000@gmail.com>
2022-01-21 17:02:15 +0530

    Committing changes

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path= require('path');
 const app=express();
 const port=80;
 
-const staticPath=path.join(__dirname,"./src");
+// const staticPath=path.join(__dirname,"./src");
 
 //built-in middleware
-app.use(express.static(staticPath));
+app.use(express.static(__dirname));
 
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./src/home.html"));
+    res.sendFile(path.join(__dirname,"./home.html"));
 });
 
 app.listen(port,()=>{
